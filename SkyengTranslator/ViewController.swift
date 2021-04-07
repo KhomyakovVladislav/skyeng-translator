ee8ad3286fd8b2a763a3736799920fbbca4a4774
Vladislav Khomyakov <vkhomyakov@devexperts.com>
2021-04-07 18:39:37 +0300

    Initial Commit

---
+++ SkyengTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyengTranslator
+//
+//  Created by Vladislav Khomyakov on 07.04.2021.
+//  Copyright © 2021 Vladislav Khomyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
